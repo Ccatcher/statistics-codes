fa094917512c5a3327626cb7e20b1ec76260b884
Ccatcher <yaowangyj@163.com>
2020-05-13 00:08:30 +0800

    First: draw the normal Possibolity Dense Function graph

--- normal_distribution_graph.py
+++ normal_distribution_graph.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-a=[1,2,3]
-d=[2,3,4]
-b=[2,3,4]
+def normalFun(x,mu,sigma):
+	pdf=np.exp(-(x-mu)**2/(2*sigma**2))/(sigma*np.sqrt(2*np.pi))
+	return pdf
+
+x=np.arange(-15,15,0.001)
+y=normalFun(x, 0, 1)
+sump=0
+alpha=0
+for p in y[::-1]:
+	sump+=p
+	alpha+=1
+	if sump>=50:		
+		break
+
+print(x[-alpha], ...)
+plt.plot(x,y)
+plt.show()
